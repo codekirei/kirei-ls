d9d2cf83a2de0cbc3c1d623aa8bd06020d34109e
Jacob Blakely <jacob@codekirei.com>
2016-02-09 20:37:58 -0800

    simplify cli

--- cli.js
+++ cli.js
@@ -2,20 +2,9 @@
 
 'use strict'
 
-const Liftoff = require('liftoff')
-const argv = require('minimist')(process.argv.slice(2))
+const flags = require('minimist')(process.argv.slice(2))
 const cli = require('./lib/cli')
 
-new Liftoff(
-  { name: 'kls'
-  , configName: '.kls'
-  , extensions:
-    { rc: null
-    }
-  , completions: null
-  }
-).launch(
-  { configPath: argv.config || argv.conf
-  }
-  , env => cli(env.configPath, argv).then(console.log)
-)
+cli(flags)
+  .then(console.log)
+  .catch(err => console.error(err.stack))
