dcc337f18a59122bdf3e50bd9c8cb87a5dc50ad2
Jacob Blakely <jacob@codekirei.com>
2016-02-11 17:57:20 -0800

    consolidate cli logic in top-level cli.js

--- cli.js
+++ cli.js
@@ -2,9 +2,33 @@
 
 'use strict'
 
-const flags = require('minimist')(process.argv.slice(2))
-const cli = require('./lib/cli')
+// import
+//----------------------------------------------------------
+const EOL   = require('os').EOL
+const argv  = require('minimist')(process.argv.slice(2))
+const co    = require('co')
+const merge = require('lodash.merge')
 
-cli(flags)
+// local
+const Output   = require('./lib/output')
+const conf     = require('./lib/conf')
+const helpText = require('./lib/helpText')
+const kls      = require('.')
+
+// jsdoc
+function* cli(flags) {
+
+  if (flags.help || flags.h) return helpText
+
+  const contents = flags._.length
+    ? yield kls(flags._)
+    : yield kls(['./'])
+
+  const _conf = merge({}, yield conf(), flags)
+
+  return new Output(contents, _conf).out.join(EOL)
+}
+
+co(cli(argv))
   .then(console.log)
   .catch(err => console.error(err.stack))
